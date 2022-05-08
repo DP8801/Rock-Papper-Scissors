38e63e7602f40497fcb1118d407eed61e5da5b55
Dev Pandya <71974172+DP8801@users.noreply.github.com>
2022-05-08 14:39:30 +0530

    HTML CSS JS added

---
+++ app.js
@@ -0,0 +1,85 @@
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const computerImage = document.getElementById('computer-image'); 
+const userChoiceDisplay = document.getElementById('user-choice');
+const userImage = document.getElementById('user-image');
+const imageLink = "Images/"; 
+const ext = ".svg"; 
+const resultDisplay = document.getElementById('result');
+const possibleChoices = document.querySelectorAll('button');
+let userChoice;
+let computerChoice;
+let result;
+
+// possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click',(e) => {
+//     userChoice = e.target.id;
+//     userChoiceDisplay.innerHTML = userChoice;
+//     generateComputerChoice();
+//     getResult();
+// }));
+
+possibleChoices.forEach(function(possibleChoice) { 
+    possibleChoice.addEventListener('click',function(e){
+        userChoice = e.target.id;
+        userChoiceDisplay.innerHTML = userChoice;
+        userImage.setAttribute("src",generateImage(userChoice));
+        computerChoice = generateComputerChoice();
+        computerChoiceDisplay.innerHTML = computerChoice;
+        computerImage.setAttribute("src",generateImage(computerChoice));
+        getResult();
+    })
+});
+
+function generateImage(choice){
+    if(choice === "rock"){
+        return imageLink+"fist"+ext;
+    } 
+    else if(choice === "scissors"){
+        return imageLink+"two-copy"+ext;
+    }
+    else if(choice === "paper"){
+        return imageLink+"privacy"+ext;
+    }
+}
+
+function generateComputerChoice(){
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length )+ 1;
+
+    if(randomNumber === 1){
+        computerChoice = "rock";
+        return computerChoice;
+    }
+    if(randomNumber === 2){
+        computerChoice = "scissors";
+        return computerChoice;
+    }
+    if(randomNumber === 3){
+        computerChoice = "paper";
+        return computerChoice;
+    }
+    
+}
+
+function getResult(){
+    if(computerChoice === userChoice){
+        result = "its a draw!";
+    }
+    if(computerChoice === 'rock' && userChoice === 'paper'){
+        result = "you win!";
+    }
+    if(computerChoice === 'rock' && userChoice === 'scissors'){
+        result = "you lost!";
+    }
+    if(computerChoice === 'paper' && userChoice === 'scissors'){
+        result = "you win!";
+    }
+    if(computerChoice === 'paper' && userChoice === 'rock'){
+        result = "you lost!";
+    }
+    if(computerChoice === 'scissors' && userChoice === 'paper'){
+        result = "you lost!";
+    }
+    if(computerChoice === 'scissors' && userChoice === 'rock'){
+        result = "you win!";
+    }
+    resultDisplay.innerHTML = result.toUpperCase();
+}
